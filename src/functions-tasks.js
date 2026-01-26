87013c9c52cdf0c30d64d6c6a1b5e382066ce0da
oreopk <sart6zaV@yandex.ru>
2026-01-26 23:28:31 +0300

    feat: done memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -123,8 +123,14 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+  return () => {
+    if (!result) {
+      result = func();
+    }
+    return result;
+  };
 }
 
 /**
