69497c163ee1da08dde74ccd2fa616ad23edf6d5
oreopk <sart6zaV@yandex.ru>
2026-01-27 01:42:08 +0300

    feat: done getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -231,8 +231,13 @@ function partialUsingArguments(fn, ...args) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let countId = startFrom;
+  return () => {
+    const currentId = countId;
+    countId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
